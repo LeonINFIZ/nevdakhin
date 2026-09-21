ae9ac2716b258c26ab3b8049e2f630d01f8ee905
Артем Топалов <feofan.neyromonah@gmail.com>
2026-09-21 22:45:00 +0300

    fix: change port to 3001 to avoid conflict with fotonadom.rf site

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: 'nevdakhin',
       script: 'node_modules/next/dist/bin/next',
-      args: 'start -p 3000',
+      args: 'start -p 3001',
       cwd: './',
       instances: 1,
       autorestart: true,
@@ -11,7 +11,7 @@ module.exports = {
       max_memory_restart: '500M',
       env: {
         NODE_ENV: 'production',
-        PORT: 3000,
+        PORT: 3001,
       },
     },
   ],
